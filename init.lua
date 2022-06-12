28b760e22b059321618aadffa38170778eb23033
delta <darkussdelta@gmail.com>
2022-06-12 21:53:28 +0200

    First push

---
+++ init.lua
@@ -0,0 +1,17 @@
+--mod-version:3.0
+local config = require "core.config"
+-- local common = require "core.common"
+
+-- TODO: Add config support
+-- Options:
+-- keep_plugins: <bool> -- Whether to keep plugins installed after delinking them | DEFAULT: true
+-- plugins: <table: <string>=<float/int> > -- List of enabled plugins
+
+config.plugins.bulb = {
+    
+}
+
+---@param string plugin
+-- function bulb(plugin)
+    
+-- end
